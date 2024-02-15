7a2dfeb06904141c2fea2212dcf9eda2929a9c2f
codexuz <142141576+codexuz@users.noreply.github.com>
2024-02-15 07:55:20 +0000

    "main"

--- android/app/src/main/java/vercel/multiexam/app/MainActivity.java
+++ android/app/src/main/java/vercel/multiexam/app/MainActivity.java
@@ -1,16 +1,21 @@
 package vercel.multiexam.app;
 
+import android.os.Bundle; // Importing Bundle class
+import java.util.ArrayList; // Importing ArrayList class
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.GoogleAauth.GoogleAuth;
+import com.getcapacitor.Plugin; // Importing Plugin class
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
-	@Override
-	public void OnCreate(Bundle savedInstanceState){
-		super.OnCreate(savedInstanceState)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		//Init Bridge
-	   this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>(){{
-         add(GoogleAuth.class)
-	   }})
-	}
+        // Init Bridge
+        this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {
+            {
+                add(GoogleAuth.class);
+            }
+        });
+    }
 }
