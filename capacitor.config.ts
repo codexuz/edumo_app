520b5c8ff3efe36de65c066898ddf0b631d8fc67
codexuz <142141576+codexuz@users.noreply.github.com>
2024-04-08 09:25:41 +0500

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "uz.edumo.academy",
+  appId: "uz.speakup.academy",
   appName: "Edumo Academy",
   webDir: "dist",
   bundledWebRuntime: false,
